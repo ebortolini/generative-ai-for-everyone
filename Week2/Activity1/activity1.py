1ccd1f085793be11b4eb5d2953046d85acd935ab
Eduardo Bortolini <ebortolini@gmail.com>
2024-07-07 21:18:42 -0300

    [Week 2] Activity 1
    # Creates a simple recommendation script

---
+++ Week2/Activity1/activity1.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+def llm_response(prompt):
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=[{'role':'user','content':prompt}],
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+prompt = '''
+    Classify the following review 
+    as having either a positive or
+    negative sentiment:
+
+    The banana pudding was really tasty!
+'''
+
+response = llm_response(prompt)
+print(response)
